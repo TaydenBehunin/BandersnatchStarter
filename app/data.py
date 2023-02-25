ff2ce02b2bf6d4b6cede292b42e50bf1c6533496
TaydenBehunin <taydenbehunin1@gmail.com>
2023-02-24 18:08:59 -0700

    Completed First ticket

--- app/data.py
+++ app/data.py
@@ -1,5 +1,7 @@
 from os import getenv
+from typing import Optional, Dict
 
+import pandas as pd
 from certifi import where
 from dotenv import load_dotenv
 from MonsterLab import Monster
@@ -8,18 +10,39 @@ from pymongo import MongoClient
 
 
 class Database:
+    load_dotenv()
+    database = MongoClient(getenv("MONGO_URL"), tlsCAFILE=where())["Database"]
 
-    def seed(self, amount):
-        pass
+    def __init__(self, collection: str):
+        self.collection = self.database[collection]
+
+    def seed(self, amount=1000):
+        """Randomly Generate a list of 1000 monsters and insert them into MongoDB"""
+        df = [Monster().to_dict() for _ in range(amount)]
+        return self.collection.insert_many(df).acknowledged
 
     def reset(self):
-        pass
+        """Remove all documents from the collection"""
+        return self.collection.drop()
 
     def count(self) -> int:
-        pass
+        """Return the number of documents present in the collection"""
+        return self.collection.count_documents({})
 
-    def dataframe(self) -> DataFrame:
-        pass
+    def dataframe(self, query: Optional[Dict] = None):
+        """Return all documents present in the collection as a DataFrame"""
+        return DataFrame(list(self.collection.find(query, {"_id": False})))
 
     def html_table(self) -> str:
-        pass
+        """Convert DataFrame to HTML table or return None if the collection is empty"""
+        if self.count() == 0:
+            return 'None'
+        else:
+            return self.dataframe().to_html()
+
+
+# if __name__ == '__main__':
+#     test = Database('Database')
+#     test.reset()
+#     test.seed()
+#     print(test.count())
