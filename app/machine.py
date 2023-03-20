947055e9eb4973a229d4de1fe7142885e43c8448
TaydenBehunin <taydenbehunin1@gmail.com>
2023-03-20 17:57:51 -0600

    Completed Third ticket

--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,30 @@
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import datetime
+
+
 class Machine:
 
-    def __init__(self, df):
-        pass
+    def __init__(self, df: DataFrame):
+        self.name = "Random Forest Classifier"
+        target = df["Rarity"]
+        features = df.drop(columns=["Rarity"])
+        self.model = RandomForestClassifier(max_depth=10, random_state=42, n_estimators=75)
+        self.model.fit(features, target)
+        self.timestamp = datetime.datetime.now()
 
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, feature_basis: DataFrame):
+        prediction, *_ = self.model.predict(feature_basis)
+        confidence, *_ = self.model.predict_proba(feature_basis)
+        return prediction, max(confidence)
 
     def save(self, filepath):
-        pass
+        return joblib.dump(self, filepath)
 
     @staticmethod
     def open(filepath):
-        pass
+        return joblib.load(filepath)
 
     def info(self):
-        pass
+        return f"Base Model: {self.name}<br>Timestamp: {self.timestamp.strftime('%Y-%m-%d %I:%M:%S %p')}"
